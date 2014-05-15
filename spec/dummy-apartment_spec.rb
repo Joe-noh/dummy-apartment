fed523fbb1ab20e3f52465f2cdb2a5254e2505a0
Joe-noh <goflb.jh@gmail.com>
2014-05-15 22:24:23 +0900

    implement method to output top and room floor
    Ofcource they are always bigger than 0.
    Room_floor is smaller than or equal to top_floor.

--- spec/dummy-apartment_spec.rb
+++ spec/dummy-apartment_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe Dummy::Apartment do
+  let(:apartment) { Dummy::Apartment.generate }
 
   it 'should generate a hash' do
-    expect(Dummy::Apartment.generate).to be_a Hash
+    expect(apartment).to be_a Hash
   end
 
   it 'should have no public class method starts with "gen_"' do
@@ -12,7 +13,7 @@ describe Dummy::Apartment do
   end
 
   describe 'Building Name' do
-    let(:name){ Dummy::Apartment.generate[:building_name] }
+    let(:name){ apartment[:building_name] }
 
     it 'should not be empty' do
       expect(name).not_to be_empty
@@ -20,7 +21,7 @@ describe Dummy::Apartment do
   end
 
   describe 'Address' do
-    let(:address){ Dummy::Apartment.generate[:address] }
+    let(:address){ apartment[:address] }
 
     it 'should match address format' do
       expect(address).to match /[都道府県].+[市町村].*[0-9]/
@@ -28,10 +29,21 @@ describe Dummy::Apartment do
   end
 
   describe 'Geo' do
-    let(:geo){ Dummy::Apartment.generate[:geo] }
+    let(:geo){ apartment[:geo] }
 
     it 'should be a couple of Float' do
       expect(geo.map(&:class)).to eql [Float, Float]
+    end
+  end
+
+  describe 'Room Floor and Top Floor' do
+    let(:top_floor) { apartment[:top_floor]  }
+    let(:room_floor){ apartment[:room_floor] }
+
+    it 'should have valid floor information' do
+      expect( top_floor).to be >= room_floor
+      expect( top_floor).to be >= 1
+      expect(room_floor).to be >= 1
     end
   end
 end
